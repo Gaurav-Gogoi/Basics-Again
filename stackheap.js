698b333f339d171b6d67d7b01f03fc879be9ebc7
Gaurav Gogoi <gauravgogoi072@gmail.com>
2025-08-25 08:50:20 +0530

    stack and heap done

---
+++ stackheap.js
@@ -0,0 +1,35 @@
+
+// stack example
+// primitive data are stored in stack and stack gives a shallow copy so when we store it in difffernt variable we only get the copy so any chnages it remains inside the copy not on the original one
+// primitive data types are:
+// Number
+// null
+// undefined
+// BigInt
+// Symbol
+// String
+// Boolean
+// Date
+
+// let name = "gaurav"
+
+// let newname = name
+// newname = "jawa"
+// console.log(newname);
+
+// console.log(name);
+
+// heap
+// so in heap when we take refernce and store it in another variable we get access to the real one so any chnages made it gets refelcted in the original one because we are able to access the original one which is also called as deep copy 
+
+// let userone = {
+//     email:"taurussilver@gmail.com",
+//     state: "assam"
+
+// }
+// let usertwo = userone
+
+// usertwo.state = "delhi"
+
+// console.log(userone);
+
